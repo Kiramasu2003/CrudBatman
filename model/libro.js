cdef63cb367387fcd7c2f33a044c137c25b28abf
Kiramasu2003 <sarcer1800@alumno.ipn.mx>
2021-10-13 04:15:20 -0500

    6 eliminar-editar

--- model/libro.js
+++ model/libro.js
@@ -8,6 +8,17 @@ module.exports = {
 
         con.query("INSERT INTO libros (titulo, autor, editorial) VALUES(?, ?, ?);", [datos.titulo, datos.autor, datos.editorial], funcion)
 
+    },
+    retornarDatosID: function (con, id, funcion) {
+        con.query("SELECT * FROM libros WHERE id = ?", [id], funcion);
+    },
+    borrar: function (con, id, funcion) {
+        con.query("DELETE FROM libros WHERE id = ?", [id], funcion);
+    },
+    actualizar: function (con, datos, funcion) {
+        con.query("UPDATE libros SET titulo=?, autor=?, editar=?"
+            + "\n  WHERE id = ?", [datos.titulo, datos.autor, datos.editorial, datos.id], funcion);
     }
+
 
 }
